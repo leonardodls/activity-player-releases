98bbf4817a8f4013b9c5d602e8166b0c3a485d8b
udit.maheshwari@gmail.com <udit.maheshwari@comrpotechnologies.com>
2020-05-01 14:33:38 +0530

    lo update

--- LO/data/consumerConfig.js
+++ LO/data/consumerConfig.js
@@ -1,78 +1,40 @@
 var consumerConfig = {
   "renderers": {
+    "overrides": [
+      {
+        "type": "activity",
+        "model": "bank",
+        "override": "test-player"
+      },
+      {
+        "type": "item",
+        "model": "spreadsheet-question",
+        "override": "spreadsheet-question-player"
+      },
+      {
+        "type": "item",
+        "model": "spreadsheet-presentation",
+        "override": "spreadsheet-presentation-player"
+      }
+    ],
     "modules": {
-      "itembank-player": {
+      "test-player": {
         "baseURL": "https://sdk-qa.leonardodls.com/modules/multi-item-activity-player/releases/",
-        "version": "0.0.30",
-        "js": "0.0.30/multiItemActivityPlayer",
-        "css": "0.0.30/multiItemActivityPlayer"
+        "version": "0.0.28",
+        "js": "0.0.28/multiItemActivityPlayer",
+        "css": "0.0.28/multiItemActivityPlayer"
       },
-      "imagelabel-dnd": {
-        "baseURL": "https://sdk-qa.leonardodls.com/modules/image-label-dnd-player/releases/",
-        "version": "0.0.31",
-        "js": "0.0.31/libs-imagelabel-dnd-item-player",
-        "dependencies": [
-          "react",
-          "react-dom",
-          "libs-player-ui-components",
-          "react-dnd",
-          "react-dnd-html5-backend"
-        ]
+      "spreadsheet-question-player": {
+        "baseURL": "https://sdk-qa.leonardodls.com/modules/spreadsheet-question-player/releases/",
+        "version": "0.7.6",
+        "js": "0.7.6/libs-spr-question-item-player.min",
+        "css": "0.7.6/libs-spr-question-item-player.min"
       },
-      "mcsr-radio": {
-        "baseURL": "https://sdk-qa.leonardodls.com/modules/mcq-radio-player/releases/",
-        "version": "0.0.32",
-        "js": "0.0.32/libs-mcsr-radio-item-player",
-        "dependencies": [
-          "react",
-          "react-dom",
-          "libs-player-ui-components"
-        ]
-      },
-      "fib-dnd": {
-        "baseURL": "https://sdk-qa.leonardodls.com/modules/fib-dnd-player/releases/",
-        "version": "0.0.20",
-        "js": "0.0.20/libs-fib-dnd-item-player",
-        "dependencies": [
-          "react",
-          "react-dom",
-          "libs-player-ui-components",
-          "react-dnd",
-          "react-dnd-html5-backend"
-        ]
-      },
-      "order-list": {
-        "baseURL": "https://sdk-qa.leonardodls.com/modules/order-list-player/releases/",
-        "version": "0.0.8",
-        "js": "0.0.8/libs-order-list-item-player",
-        "dependencies": [
-          "react",
-          "react-dom",
-          "libs-player-ui-components"
-        ]
-      },
-      "react": {
-        "baseURL": "https://sdk-qa.leonardodls.com/modules/react/releases/",
-        "version": "16.12.0",
-        "js": "16.12.0/react.development"
-      },
-      "react-dom": {
-        "baseURL": "https://sdk-qa.leonardodls.com/modules/react-dom/releases/",
-        "version": "16.12.0",
-        "js": "16.12.0/react-dom.development"
-      },
-      "react-dnd": {
-        "baseURL": "https://cdn.jsdelivr.net/npm/react-dnd@10.0.2/dist/umd/",
-        "js": "ReactDnD.min"
-      },
-      "react-dnd-html5-backend": {
-        "baseURL": "https://cdn.jsdelivr.net/npm/react-dnd-html5-backend@10.0.2/dist/umd/",
-        "js": "ReactDnDHTML5Backend.min"
-      },
-      "libs-player-ui-components": {
-        "baseURL": "https://sdk-qa.leonardodls.com/modules/libs-player-ui-components/releases/",
-        "version": "0.0.20",
-        "js": "0.0.20/libs-player-ui-components"
+      "spreadsheet-presentation-player": {
+        "baseURL": "https://sdk-qa.leonardodls.com/modules/spreadsheet-presentation-player/releases/",
+        "version": "0.7.5",
+        "js": "0.7.5/libs-spr-presentation-item-player.min",
+        "css": "0.7.5/libs-spr-presentation-item-player.min"
       }
     }
   }
