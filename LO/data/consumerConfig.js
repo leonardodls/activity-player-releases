a6f83a2edf1db16e44c8851840da18e3ec4430d7
udit.maheshwari@gmail.com <udit.maheshwari@comrpotechnologies.com>
2020-05-01 14:39:43 +0530

    adding dependency

--- LO/data/consumerConfig.js
+++ LO/data/consumerConfig.js
@@ -28,13 +28,17 @@ var consumerConfig = {
         "baseURL": "https://sdk-qa.leonardodls.com/modules/spreadsheet-question-player/releases/",
         "version": "0.7.6",
         "js": "0.7.6/libs-spr-question-item-player.min",
-        "css": "0.7.6/libs-spr-question-item-player.min"
+        "css": "0.7.6/libs-spr-question-item-player.min",
+        "dependencies": ["jquery"]
       },
       "spreadsheet-presentation-player": {
         "baseURL": "https://sdk-qa.leonardodls.com/modules/spreadsheet-presentation-player/releases/",
         "version": "0.7.5",
         "js": "0.7.5/libs-spr-presentation-item-player.min",
         "css": "0.7.5/libs-spr-presentation-item-player.min"
+      },
+      "jquery": {
+        "js": "https://sdk-qa.s3.us-east-2.amazonaws.com/modules/jquery/releases/3.1.1/jquery-3.1.1"
       }
     }
   }
